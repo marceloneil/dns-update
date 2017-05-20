a235ac92bb035c96327508e461b7794f7219c10f
Marcel O'Neil <marcel@marceloneil.com>
2017-05-20 16:05:19 -0400

    Formatting

--- dns-update.py
+++ dns-update.py
@@ -4,12 +4,12 @@ from dnsimple import DNSimple
 dns = DNSimple()
 ip = request.urlopen('https://ifconfig.co/ip').read().decode('utf-8').rstrip()
 data = {'content': ip}
-domain = 'example.com' # Change this!
+domain = 'example.com'  # Change this!
 
 for item in dns.records(domain):
-	if item['record']['record_type'] == 'A':
-		if item['record']['content'] != ip:
-			old_ip = item['record']['content']
-			id = item['record']['id']
-			dns.update_record(domain, id, data)
-			print('Updated', old_ip, 'to', ip)
+    if item['record']['record_type'] == 'A':
+        if item['record']['content'] != ip:
+            old_ip = item['record']['content']
+            id = item['record']['id']
+            dns.update_record(domain, id, data)
+            print('Updated', old_ip, 'to', ip)
